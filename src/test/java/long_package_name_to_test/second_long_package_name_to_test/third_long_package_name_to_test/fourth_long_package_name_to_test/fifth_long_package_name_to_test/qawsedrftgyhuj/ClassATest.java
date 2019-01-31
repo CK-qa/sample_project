b85db38c0dad42fcde81684ca0757b5062fda686
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2019-01-31 13:57:18 +0300

    classb

---
+++ src/test/java/long_package_name_to_test/second_long_package_name_to_test/third_long_package_name_to_test/fourth_long_package_name_to_test/fifth_long_package_name_to_test/qawsedrftgyhuj/ClassATest.java
@@ -0,0 +1,8 @@
+package long_package_name_to_test.second_long_package_name_to_test.third_long_package_name_to_test.fourth_long_package_name_to_test.fifth_long_package_name_to_test.qawsedrftgyhuj;
+
+import static org.junit.Assert.*;
+
+public class ClassATest {
+
+    //test
+}
---
+++ src/test/java/long_package_name_to_test/second_long_package_name_to_test/third_long_package_name_to_test/fourth_long_package_name_to_test/fifth_long_package_name_to_test/qawsedrftgyhuj/LongFileNameToTest.java
@@ -0,0 +1,12 @@
+package long_package_name_to_test.second_long_package_name_to_test.third_long_package_name_to_test.fourth_long_package_name_to_test.fifth_long_package_name_to_test.qawsedrftgyhuj;
+
+
+public class LongFileNameToTest {
+
+    public static void main(String[] args) {
+      String str = "C:\\Users\\viktoria.bozhko\\IdeaProjects\\sample_project\\src\\test\\java\\long_package_name_to_test\\second_long_package_name_to_test\\third_long_package_name_to_test\\fourth_long_package_name_to_test\\fifth_long_package_name_to_test\\qawsedrftgyhuj\\LongFileNameToTest.java";
+      System.out.println(str.replace("\\\\", "\\"));
+        System.out.println(str.replace("\\\\", "\\").length());
+            
+        }
+    }
