70324d6b8e23921c132c488c6092f045fe019312
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2019-02-08 13:38:37 +0300

    classb

---
+++ src/main/java/CLassU.java
@@ -0,0 +1,10 @@
+public class CLassU {
+    
+    public static boolean boo(String a, String b) {
+        return a.equals(b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(boo("pawe", "Pawe"));
+    }
+}
--- src/test/java/long_package_name_to_test/second_long_package_name_to_test/third_long_package_name_to_test/fourth_long_package_name_to_test/fifth_long_package_name_to_test/qawsedrftgyhuj/LongFileNameToTest.java
+++ src/test/java/long_package_name_to_test/second_long_package_name_to_test/third_long_package_name_to_test/fourth_long_package_name_to_test/fifth_long_package_name_to_test/qawsedrftgyhuj/LongFileNameToTest.java
@@ -4,7 +4,7 @@ package long_package_name_to_test.second_long_package_name_to_test.third_long_pa
 public class LongFileNameToTest {
 
     public static void main(String[] args) {
-      String str = "C:\\Users\\viktoria.bozhko\\IdeaProjects\\sample_project\\src\\test\\java\\long_package_name_to_test\\second_long_package_name_to_test\\third_long_package_name_to_test\\fourth_long_package_name_to_test\\fifth_long_package_name_to_test\\qawsedrftgyhuj\\LongFileNameToTest.java";
+      String str = "C:\\Users\\viktoria.bozhko\\IdeaProjects\\IntelliJTest\\code_features\\src\\main\\java\\vcsStuff\\package_with_longname\\package_with_longname_2\\package_with_longname_3\\package_with_longname_4\\package_with_longname_5\\package_with_longname_6\\package_with_longname_7\\Ttt.java";
       System.out.println(str.replace("\\\\", "\\"));
         System.out.println(str.replace("\\\\", "\\").length());
             
